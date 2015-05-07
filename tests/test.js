eacc4c0a734fd9ae895e5fcb8971a846d920f986
andrewbrandwood <andrew@brandwood.com>
2015-05-07 12:32:07 +0100

    removing jest-cli from dependencies

---
+++ tests/test.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var assert = require('assert'),
+    es = require('event-stream'),
+    gutil = require('gulp-util'),
+    PassThrough = require('stream').PassThrough,
+    sgc = require('../index');
+
+describe('sass-generate-contents', function() {
+	it('should fail in stream mode', function(done) {
+		
+
+	});
+});
